0a22fcefaa7b13f55bea36339d5fe01689d241f6
EkanSvahn <ekan.svahn@gmail.com>
2024-07-18 14:44:12 +0200

    initial commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
+        <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
+          <h1 className="text-xl"></h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Auth;
