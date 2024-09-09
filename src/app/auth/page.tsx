889af186138e47ece2bea9ef4c08e87db46488a1
EkanSvahn <ekan.svahn@gmail.com>
2024-09-09 09:05:15 +0200

    feature: implement auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,88 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+
+const defaultFormData = {
+  email: "",
+  name: "",
+  password: "",
+};
 
 const Auth = () => {
+  const [formData, setFormData] = useState(defaultFormData);
+  const inputStyle =
+    "border border-gray-300 sm:text-sm text-black rounded:lg w-full p2-.5 focus:outline-none";
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      console.log(formData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setFormData(defaultFormData);
+    }
+  };
+
   return (
     <section className="container mx-auto">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
         <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
-          <h1 className="text-xl"></h1>
+          <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+            Create an account
+          </h1>
+          <p>OR</p>
+          <span className="inline-flex items-center">
+            <AiFillGithub className="mr-3 text-4xl cursor-pointer text-black dark:text-white" />{" "}
+            | <FcGoogle className="mr-3 text-4xl cursor-pointer" />
+          </span>
         </div>
+        <form className="space-y-4 md:space-y-6 " onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="name@company.com"
+            required
+            className={inputStyle}
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="John Doe"
+            className={inputStyle}
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            minLength={6}
+            className={inputStyle}
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-tertiary-dark focus:outline-none p-2 rounded-lg text-sm px-5 py-2.5"
+          >
+            Sign Up{" "}
+          </button>
+        </form>
+        <button className="text-blue-700 underline">Login in</button>
       </div>
     </section>
   );
