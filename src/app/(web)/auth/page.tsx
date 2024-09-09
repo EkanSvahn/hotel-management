26289633a279c0da7fbff35bb696b646820301ae
EkanSvahn <ekan.svahn@gmail.com>
2024-09-09 16:48:55 +0200

    feature: re-arrange project structure -general fixes for the project structure -fix dependencies -fix configs

--- src/app/(web)/auth/page.tsx
+++ src/app/(web)/auth/page.tsx
@@ -11,7 +11,7 @@ const defaultFormData = {
 
 const Auth = () => {
   const [formData, setFormData] = useState(defaultFormData);
-  const inputStyle =
+  const inputStyles =
     "border border-gray-300 sm:text-sm text-black rounded:lg w-full p2-.5 focus:outline-none";
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -32,54 +32,61 @@ const Auth = () => {
   };
 
   return (
-    <section className="container mx-auto">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
-        <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
-          <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+    <section className='container mx-auto'>
+      <div className='p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto'>
+        <div className='flex mb-8 flex-col md:flex-row items-center justify-between'>
+          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl'>
             Create an account
           </h1>
           <p>OR</p>
-          <span className="inline-flex items-center">
-            <AiFillGithub className="mr-3 text-4xl cursor-pointer text-black dark:text-white" />{" "}
-            | <FcGoogle className="mr-3 text-4xl cursor-pointer" />
+          <span className='inline-flex items-center'>
+            <AiFillGithub
+              // onClick={loginHandler}
+              className='mr-3 text-4xl cursor-pointer text-black dark:text-white'
+            />{' '}
+            |
+            <FcGoogle
+              // onClick={loginHandler}
+              className='ml-3 text-4xl cursor-pointer'
+            />
           </span>
         </div>
-        <form className="space-y-4 md:space-y-6 " onSubmit={handleSubmit}>
+
+        <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
           <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="name@company.com"
+            type='email'
+            name='email'
+            placeholder='name@company.com'
             required
-            className={inputStyle}
+            className={inputStyles}
             value={formData.email}
             onChange={handleInputChange}
           />
           <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="John Doe"
-            className={inputStyle}
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            required
+            className={inputStyles}
             value={formData.name}
             onChange={handleInputChange}
           />
           <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="password"
+            type='password'
+            name='password'
+            placeholder='password'
+            required
             minLength={6}
-            className={inputStyle}
+            className={inputStyles}
             value={formData.password}
             onChange={handleInputChange}
           />
 
           <button
-            type="submit"
-            className="w-full bg-tertiary-dark focus:outline-none p-2 rounded-lg text-sm px-5 py-2.5"
+            type='submit'
+            className='w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center'
           >
-            Sign Up{" "}
+            Sign Up
           </button>
         </form>
         <button className="text-blue-700 underline">Login in</button>
